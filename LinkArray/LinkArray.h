07b36a857a7fcbb2765897be3bdd1ba5e49207e8
yudongxuer <84443307+yudongxuer@users.noreply.github.com>
2021-05-24 11:27:47 +0800

    Create LinkArray.h

---
+++ LinkArray/LinkArray.h
@@ -0,0 +1,22 @@
+#ifndef CPPEXAM_LINKARRAY_H
+#define CPPEXAM_LINKARRAY_H
+#include "Node.h"
+
+
+class LinkArray {
+private:
+    int size;
+    Node *linkArray;
+    Node *tail;
+public:
+    int Size();
+    bool Append(int value);
+    int Index(int index);
+    bool Set(int index, int value);
+    bool Delete(int index);
+    Node *GetFirstPointer();
+    LinkArray();
+};
+
+
+#endif //CPPEXAM_LINKARRAY_H
